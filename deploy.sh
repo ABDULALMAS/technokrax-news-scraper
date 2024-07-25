dd656c20831002db46012818f4ad422da697ae0c
Abdul Almas <abdulalmas696@gmail.com>
2024-07-25 13:38:36 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "command - npx tsc"
+npx tsc
+
+echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654573394.dkr.ecr.us-west-2.amazonaws.com"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654573394.dkr.ecr.us-west-2.amazonaws.com
+
+echo "command - docker build --platform linux/amd64 --progress=plain -t technokrax-news-scraper ."
+docker build --platform linux/amd64 --progress=plain -t technokrax-news-scraper .
+
+echo "command - docker tag scraper:latest <aws-account-id>.dkr.ecr.us-west-2.amazonaws.com/technokrax-news-scraper:latest"
+docker tag scraper:latest 654654573394.dkr.ecr.us-west-2.amazonaws.com/technokrax-news-scraper:latest
+
+echo "command - docker push 654654573394.dkr.ecr.us-west-2.amazonaws.com/technokrax-news-scraper:latest"
+docker push 654654573394.dkr.ecr.us-west-2.amazonaws.com/technokrax-news-scraper:latest
